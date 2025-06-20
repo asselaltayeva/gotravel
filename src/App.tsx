135b41099cd13be24f5c0fca595c571bbbd45506
Assel <asselaltayeva@users.noreply.github.com>
2025-06-20 23:57:33 +0900

    Steps, Rating added

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,20 @@ import Header from "./components/Header"
 import Navigation from "./components/Navigation/Navigation";
 import Hero from "./components/Hero/Hero";
 
+//main
+import Main from "./components/Main";
+import Steps from "./components/Steps/Steps";
+
 function App() {
   return <Page>
     <Header>
       <Navigation />
       <Hero />
     </Header>
+
+    <Main>
+      <Steps/>
+    </Main>
   </Page>;
 }
 
