32da5b4b5177bd4b63ea0c26964ab2c5e7b6987e
Assel <asselaltayeva@users.noreply.github.com>
2025-06-20 20:58:51 +0900

    Navigation added

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+//page
+import Page from "./components/Page"
+import Header from "./components/Header"
+import Navigation from "./components/Navigation/Navigation";
+
+function App() {
+  return <Page>
+    <Header>
+      <Navigation />
+    </Header>
+  </Page>;
+}
+
+export default App;
---
+++ src/components/Page.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+
+interface PageProps {
+    children: ReactElement;
+}
+
+export default function Page(props: PageProps) {
+  return (
+    <div>{props.children}</div>
+  )
+}
